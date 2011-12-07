01c789811d352a7019a47447e1e4df5296852f28
Joel VanderWerf <vjoel@users.sourceforge.net>
2011-12-06 21:05:56 -0800

    remove workaround code for obsolete ruby version

--- lib/fsdb/file-lock.rb
+++ lib/fsdb/file-lock.rb
@@ -7,10 +7,6 @@ class File
   CAN_DELETE_OPEN_FILE  = !FSDB::PLATFORM_IS_WINDOWS
   CAN_OPEN_DIR          = !FSDB::PLATFORM_IS_WINDOWS
   
-  LOCK_BLOCK_FIXED_VER  = "1.8.2" # Hurray!
-  LOCK_DOESNT_BLOCK     = [RUBY_VERSION, LOCK_BLOCK_FIXED_VER].
-    map {|s| s.split('.')}.sort[0].join('.') ==  LOCK_BLOCK_FIXED_VER
-
   if FSDB::PLATFORM_IS_WINDOWS_ME
     # no flock() on WinME
 
@@ -21,61 +17,22 @@ class File
     end
   
   else
-    if LOCK_DOESNT_BLOCK
-      # Get an exclusive (i.e., write) lock on the file.
-      # If the lock is not available, wait for it without blocking other ruby
-      # threads.
-      def lock_exclusive lock_type
-        send(lock_type, LOCK_EX)
-      rescue Errno::EINTR
-        retry
-      end
+    # Get an exclusive (i.e., write) lock on the file.
+    # If the lock is not available, wait for it without blocking other ruby
+    # threads.
+    def lock_exclusive lock_type
+      send(lock_type, LOCK_EX)
+    rescue Errno::EINTR
+      retry
+    end
 
-      # Get a shared (i.e., read) lock on the file.
-      # If the lock is not available, wait for it without blocking other ruby
-      # threads.
-      def lock_shared lock_type
-        send(lock_type, LOCK_SH)
-      rescue Errno::EINTR
-        retry
-      end
-
-    else
-      def lock_exclusive lock_type
-        if Thread.list.size == 1
-          begin
-            send(lock_type, LOCK_EX)
-          rescue Errno::EINTR
-            retry
-          end
-        else
-          # ugly hack because waiting for a lock in a Ruby thread blocks the
-          # entire process
-          period = 0.001
-          until send(lock_type, LOCK_EX|LOCK_NB)
-            sleep period
-            period *= 2 if period < 1
-          end
-        end
-      end
-
-      def lock_shared lock_type
-        if Thread.list.size == 1
-          begin
-            send(lock_type, LOCK_SH)
-          rescue Errno::EINTR
-            retry
-          end
-        else
-          # ugly hack because waiting for a lock in a Ruby thread blocks the
-          # entire process
-          period = 0.001
-          until send(lock_type, LOCK_SH|LOCK_NB)
-            sleep period
-            period *= 2 if period < 1
-          end
-        end
-      end
+    # Get a shared (i.e., read) lock on the file.
+    # If the lock is not available, wait for it without blocking other ruby
+    # threads.
+    def lock_shared lock_type
+      send(lock_type, LOCK_SH)
+    rescue Errno::EINTR
+      retry
     end
   end
   
